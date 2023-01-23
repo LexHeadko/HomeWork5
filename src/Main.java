cd3b3be50443ecd9eeb0f5bd538d8c1a64512aaa
LexHeadko <n-odes11rus@yandex.ru>
2023-01-23 21:34:44 +0200

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,14 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        byte clientOS = 0;
+        byte clientOS = 5;
         // 0 - iOS, 1 - AndroidOS
-
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Ошибка");
+        }
     }
 }
