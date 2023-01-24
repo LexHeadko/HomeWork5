4b66af8b094e3bcc71f2cf6aa934d3307245bb0b
LexHeadko <n-odes11rus@yandex.ru>
2023-01-24 12:08:44 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
-        task4();
+        //task4();
         //task5();
         //task6();
         //task7();
@@ -24,32 +24,25 @@ public class Main {
     }
     public static void task2() {
         System.out.println("Задача 2");
-        int yearOfRelease = 2013;
-        byte clientOS = 1; // 0 - iOS, 1 - AndroidOS
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке");
-                break;
-            default:
-                System.out.println("Ошибка определения ОС");
-        }
+        int yearOfRelease = 2052;
+        byte clientOS = 0; // 0 - iOS, 1 - AndroidOS
         if (clientOS == 0 && yearOfRelease < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 1 && yearOfRelease < 2015) {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && yearOfRelease > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && yearOfRelease >2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
     public static void task3() {
         System.out.println("Задача 3");
         int year = 2021;
     }
-    public static void task4() {
-        System.out.println("Задача 4");
-    int deliveryDistance = 95;
-    if (deliveryDistance > 100) {
-        System.out.println("Доставки нет!");
+    //public static void task4() {
+      //  System.out.println("Задача 4");
+    //int deliveryDistance = 95;
+    //if (deliveryDistance > 100) {
+    //    System.out.println("Доставки нет!");
     }
-}
