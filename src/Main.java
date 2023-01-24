6d4ece4e327e85393f513cc38246bbf99c068fcf
LexHeadko <n-odes11rus@yandex.ru>
2023-01-24 06:36:25 +0200

    Задача 3,4

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        //task3();
-        //task4();
+        task3();
+        task4();
         //task5();
         //task6();
         //task7();
@@ -41,5 +41,15 @@ public class Main {
         } else if (clientOS == 1 && yearOfRelease < 2015) {
                     System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2021;
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+    int deliveryDistance = 95;
+    if (deliveryDistance > 100) {
+        System.out.println("Доставки нет!");
     }
 }
